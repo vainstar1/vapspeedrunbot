a4ce69e354e53a8cb519e8522b0505e98af44bb8
vainstar1 <andrewreyes112211@gmail.com>
2024-08-22 00:45:23 -0400

    whar

---
+++ speedrun.py
@@ -0,0 +1,44 @@
+import discord
+import os
+import time
+from discord.ext import commands
+from discord import app_commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+cooldown_time = 1800 
+last_used = 0 
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} commands")
+    except Exception as e:
+        print(f"Error syncing commands: {e}")
+
+@bot.event
+async def on_message(message):
+    global last_used
+    current_time = time.time()
+
+    if "speedrun" in message.content.lower():
+        if current_time - last_used >= cooldown_time:
+            last_used = current_time
+            await message.channel.send(file=discord.File('speedrun.gif'))
+        else:
+            remaining_time = int(cooldown_time - (current_time - last_used))
+            print("on cooldown gangy wangy")
+
+    await bot.process_commands(message)
+
+bot.run(TOKEN)
